f3c9d0bb792306e89a71f78429a55f2cbfd98ede
MexcainJaguar <mariojesusmartinez@hotmail.com>
2022-07-11 12:55:33 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Configuration;
+
+namespace Availity_CSVProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read source file, assign destination folder, both defined in the App.Config file
+
+            string inputFile = ConfigurationSettings.AppSettings["source"].ToString();
+            string destFolder = ConfigurationSettings.AppSettings["destination"].ToString();
+
+            //Call Method to separate rows into different files
+            CreateSeparateFileByHI(inputFile, destFolder);
+        }
+
+        public static void CreateSeparateFileByHI(string inputCSV, string destinationPath)
+        {
+            //Declare generic list of InsuranceClient class
+            List<InsuranceClient> lstClients = new List<InsuranceClient>();
+
+            //Extract elements from CSV file and add them to List
+            using (var reader = new StreamReader(inputCSV))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    string[] values = line.Split(',');
+                    InsuranceClient insClient = new InsuranceClient();
+                    insClient.UserId = Convert.ToInt32(values[0].ToString());
+                    insClient.FName = values[1].ToString();
+                    insClient.LName = values[2].ToString();
+                    insClient.Version = Convert.ToInt32(values[3].ToString());
+                    insClient.InsuranceCompany = values[4].ToString();
+                    lstClients.Add(insClient);
+                }
+            }
+
+            //Select unique insurance companies and sort them alphabetically.
+            List<string> insCompanies = lstClients.Select(x => x.InsuranceCompany).Distinct().ToList();
+            insCompanies.Sort();
+
+            //Loop though each Insurance company
+            foreach (string comp in insCompanies)
+            {
+                //List<InsuranceClient> newSet = (from x in lstClients where x.InsuranceCompany == comp select x).ToList();
+
+                List<InsuranceClient> uniqueIds = lstClients.GroupBy(p => p.UserId).Select(g => g.Last()).ToList();
+                List<InsuranceClient> uniqueVer = (from x in uniqueIds where x.InsuranceCompany == comp select x).ToList();
+                List<InsuranceClient> newSet = uniqueVer.OrderBy(x => x.LName).ToList();
+
+
+                string fileName = destinationPath + comp + ".CSV";
+
+                using (var file = File.CreateText(fileName))
+                {
+                    foreach (InsuranceClient item in newSet)
+                    {
+                        StringBuilder newCSVRow = new StringBuilder();
+
+                        newCSVRow.Append(item.UserId + "," + item.FName + "," + item.LName + "," + item.Version + "," + item.InsuranceCompany);
+
+                        file.WriteLine(newCSVRow.ToString());
+                    }
+                }
+
+
+            }
+        }
+    }
+
+    public class InsuranceClient
+    {
+        private int _userid;
+        private string _fname;
+        private string _lname;
+        private int _version;
+        private string _insuranceCompany;
+
+
+        public int UserId
+        {
+            get { return _userid; }
+            set { _userid = value; }
+        }
+        public string FName
+        {
+            get { return _fname; }
+            set { _fname = value; }
+        }
+        public string LName
+        {
+            get { return _lname; }
+            set { _lname = value; }
+        }
+        public int Version
+        {
+            get { return _version; }
+            set { _version = value; }
+        }
+        public string InsuranceCompany
+        {
+            get { return _insuranceCompany; }
+            set { _insuranceCompany = value; }
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Availity_CSVProcess")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Availity_CSVProcess")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9cb71d32-7f6e-4d15-aac8-64e478147ada")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
